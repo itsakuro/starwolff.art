a6d693119747c1a8cc38e6a2f1cd0d07074130af
Starr <shocked1370@gmail.com>
2024-01-22 11:34:37 -0600

    Initial commit, navbar final

---
+++ scripts/time.js
@@ -0,0 +1,31 @@
+const time = document.getElementById("time");
+const timeWrapper = document.getElementById("time-wrapper");
+
+let displayTime = true;
+
+function getCurrentTimeCST() {
+    const now = new Date();
+    now.toLocaleString('en-US', { timeZone: 'America/Chicago' });
+    
+    const hours = now.getHours() % 12 || 12;
+    const minutes = now.getMinutes();
+    const ampm = now.getHours() < 12 ? 'AM' : 'PM';
+    
+    const timeCST = `${hours}:${minutes < 10 ? '0' : ''}${minutes} ${ampm}`;
+    return timeCST;
+}
+
+setInterval(() => {
+    if (displayTime) {
+        time.innerHTML = getCurrentTimeCST();
+    }
+}, 1);
+
+timeWrapper.addEventListener("mouseover", () => {
+    displayTime = false;
+    time.innerHTML = "Central Time";
+});
+
+timeWrapper.addEventListener("mouseout", () => {
+    displayTime = true;
+});
---
+++ scripts/work.js
@@ -0,0 +1,38 @@
+// CURRENT HIRE STATUS
+let currentHireStatus = true; // TRUE -> available for work | FALSE -> not available
+
+const hireStatus = document.getElementById("hire-status-text");
+const hireStatusWrapper = document.getElementById("hire-status");
+const hireStatusIcon = document.getElementById("hire-status-icon");
+
+let displayHireStatus = true;
+
+function getHireStatus() {
+    if (currentHireStatus) {
+        hireStatusIcon.style.fill = "#17D0AF";
+        hireStatus.innerHTML = "Available for work";
+        if (!hireStatusWrapper.classList.contains("status-free")) {
+            hireStatusWrapper.classList.add("status-free")
+        }
+    } else {
+        hireStatusIcon.style.fill = "#FF2048";
+        hireStatus.innerHTML = "Currently not available for work";
+        if (!hireStatusWrapper.classList.contains("status-busy")) {
+            hireStatusWrapper.classList.add("status-busy")
+        }
+    }
+}
+
+getHireStatus();
+
+hireStatusWrapper.addEventListener("mouseover", () => {
+    if (currentHireStatus) {
+        displayHireStatus = false;
+        hireStatus.innerHTML = "Click here for details";
+    }
+});
+
+hireStatusWrapper.addEventListener("mouseout", () => {
+    displayHireStatus = true;
+    getHireStatus();
+});
